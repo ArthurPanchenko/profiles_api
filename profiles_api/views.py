a0d1e9e07083f0b6503f45d7a8d67748f62463a4
artur <arturrka@bk.ru>
2023-04-25 13:35:31 +0300

    Create serializer, viewset and router for profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import UserProfileSerializer
+from .models import UserProfile
+
+
+class UserProfileViewSet(ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    
+    
