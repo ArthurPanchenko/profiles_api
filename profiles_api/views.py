0d8a13b7f788edf3113174c2ca776b88c55a94ab
artur <arturrka@bk.ru>
2023-04-25 13:55:42 +0300

    Added permission class and authentication class

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from .serializers import UserProfileSerializer
+from .permissions import UpdateOwnProfile
 from .models import UserProfile
 
 
@@ -10,5 +12,6 @@ class UserProfileViewSet(ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
-    
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [UpdateOwnProfile,]
     
